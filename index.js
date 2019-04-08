2cfc66e3d2a476b0e30455d01c17b61de0a7a4ca
Alberto Duarte <betodute@gmail.com>
2019-04-08 21:13:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten () {
   kittens.push();
-  return kittens ;
 }
 
 function destructivelyPrependKitten () {
   kittens.shift();
-  return kittens ;
 }
 
 function destructivelyRemoveLastKitten () {
   kittens.pop() ;
-  return kittens ;
 }
 
 function destructivelyRemoveFirstKitten () {
   kittens.unshift();
-  return kittens ;
 }
 
